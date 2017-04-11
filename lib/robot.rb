0dc4412102fe6dd4630622fa5111d58a328f523f
Gabriella <gabriellamds@yahoo.it>
2017-04-11 12:20:05 +0100

    implemented report_final_position with TDD, made tests for when it receives all types of instructions accepted

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,4 +1,4 @@
-class Robot
+class MartianRobot
 
   attr_reader :coordinates, :orientation
 
@@ -7,17 +7,59 @@ class Robot
     @orientation = orientation
   end
 
-  def move(instruction)
-    if instruction == 'F'
-      if @orientation == 'N'
-        [@coordinates[0], (@coordinates[0]+1)]
-      else
-        [@coordinates[0], @coordinates[0]]
+  def report_final_position(instruction)
+    x = @coordinates[0]
+    y = @coordinates[1]
+    final_position = [x, y]
+    array_of_instructions = instruction.split('')
+
+    array_of_instructions.each do |single_instruction|
+      if single_instruction == 'F'
+        # move_forward(@orientation)
+        if @orientation == 'E'
+          final_position[0] += 1
+        elsif @orientation == 'W'
+          final_position[0] -= 1
+        elsif @orientation == 'N'
+          final_position[1] += 1
+        elsif @orientation == 'S'
+          final_position[1] -= 1
+        end
+      end
+      if single_instruction == 'R'
+        changed_orientation = change_orientation_clockwise(@orientation)
+        @orientation = changed_orientation
+      end
+      if single_instruction == 'L'
+        changed_orientation = change_orientation_anticlockwise(@orientation)
+        @orientation = changed_orientation
       end
     end
-    if instruction == 'R' || instruction == 'L'
-      [@coordinates[0], @coordinates[0]]
-    end 
+    final_position
+  end
+
+  def change_orientation_clockwise(initial_orientation)
+    if initial_orientation == 'N'
+      'E'
+    elsif initial_orientation == 'E'
+      'S'
+    elsif initial_orientation == 'S'
+      'W'
+    else
+      'N'
+    end
+  end
+
+  def change_orientation_anticlockwise(initial_orientation)
+    if initial_orientation == 'N'
+      'W'
+    elsif initial_orientation == 'E'
+      'N'
+    elsif initial_orientation == 'S'
+      'E'
+    else
+      'S'
+    end
   end
 
 end
---
+++ spec/robot_spec.rb
@@ -0,0 +1,86 @@
+$LOAD_PATH << File.expand_path('. ./lib', __FILE__)
+# require 'martian_robots'
+require 'robot'
+
+RSpec.describe MartianRobot do
+
+    let (:robot) {MartianRobot.new(@coordinates, @orientation)}
+    # let(:martian_robots) {MartianRobots.new}
+
+    it "returns [2,1] if robot starts from [1,1] 'E' and receives 'F'" do
+      robot = MartianRobot.new([1,1], "E")
+      expect(robot.report_final_position('F')).to eq([2,1])
+    end
+
+    it "returns [0,1] if robot starts from [1,1] 'W' and receives 'F'" do
+      robot = MartianRobot.new([1,1], "W")
+      expect(robot.report_final_position('F')).to eq([0,1])
+    end
+
+    it "returns [1,2] if robot starts from [1,1] 'N' and receives 'F'" do
+      robot = MartianRobot.new([1,1], "N")
+      expect(robot.report_final_position('F')).to eq([1,2])
+    end
+
+    it "returns [4,3] if robot starts from [4,4] 'S' and receives 'F'" do
+      robot = MartianRobot.new([4,4], "S")
+      expect(robot.report_final_position('F')).to eq([4,3])
+    end
+
+    it "returns [4,2] if robot starts from [4,4] 'S' and receives 'FF'" do
+      robot = MartianRobot.new([4,4], "S")
+      expect(robot.report_final_position('FF')).to eq([4,2])
+    end
+
+    it "returns [1,3] if robot starts from [1,1] 'N' and receives 'FF'" do
+      robot = MartianRobot.new([1,1], "N")
+      expect(robot.report_final_position('FF')).to eq([1,3])
+    end
+
+    it "returns [-2,1] if robot starts from [1,1] 'W' and receives 'FFF'" do
+      robot = MartianRobot.new([1,1], "W")
+      expect(robot.report_final_position('FFF')).to eq([-2,1])
+    end
+
+    it "returns [5,1] if robot starts from [1,1] 'E' and receives 'FFFF'" do
+      robot = MartianRobot.new([1,1], "E")
+      expect(robot.report_final_position('FFFF')).to eq([5,1])
+    end
+
+    it "returns E if robot's initial orientation is N" do
+      expect(robot.change_orientation_clockwise("N")).to eq("E")
+    end
+
+    it "returns W if robot's initial orientation is S" do
+      expect(robot.change_orientation_clockwise("S")).to eq("W")
+    end
+
+    it "returns [2,2] if robot starts from [1,1] 'N' and receives 'FRF'" do
+      robot = MartianRobot.new([1,1], "N")
+      expect(robot.report_final_position("FRF")).to eq([2,2])
+    end
+
+    it "returns [1,-1] if robot starts from [1,1] 'S' and receives 'FFFRRF'" do
+      robot = MartianRobot.new([1,1], "S")
+      expect(robot.report_final_position("FFFRRF")).to eq([1,-1])
+    end
+
+    it "returns S if robot's initial orientation is W" do
+      expect(robot.change_orientation_anticlockwise("W")).to eq("S")
+    end
+
+    it "returns N if robot's initial orientation is E" do
+      expect(robot.change_orientation_anticlockwise("E")).to eq("N")
+    end
+
+    it "returns [2,2] if robot starts from [1,1] 'E' and receives 'FLF'" do
+      robot = MartianRobot.new([1,1], "E")
+      expect(robot.report_final_position("FLF")).to eq([2,2])
+    end
+
+    it "returns [0,3] if robot starts from [1,1] 'S' and receives 'LLFFLF'" do
+      robot = MartianRobot.new([1,1], "S")
+      expect(robot.report_final_position("LLFFLF")).to eq([0,3])
+    end
+
+end
